78ef4f8fd9d511a5d370ea3a45c427e9921f5ffc
kartheek <karthikmarabathula18@gmail.com>
2026-08-02 14:08:29 +0530

    updated trust policy

--- terraform/bootstrap/iam.tf
+++ terraform/bootstrap/iam.tf
@@ -32,8 +32,15 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
 
         "repo:${var.github_owner}@${var.github_owner_id}/${var.github_repository}@${var.github_repository_id}:ref:refs/heads/main",
 
-        "repo:${var.github_owner}@${var.github_owner_id}/${var.github_repository}@${var.github_repository_id}:environment:dev",
+        "repo:${var.github_owner}@${var.github_owner_id}/${var.github_repository}@${var.github_repository_id}:environment:dev"
+      ]
+    }
 
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+
+      values = [
         "repo:${var.github_owner}@${var.github_owner_id}/${var.github_repository}@${var.github_repository_id}:ref:refs/heads/feature/*"
       ]
     }
