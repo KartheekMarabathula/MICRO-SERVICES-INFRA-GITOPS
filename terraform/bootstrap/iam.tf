d10450c641c948f9129244c8847f46d27c9d1d21
kartheek <karthikmarabathula18@gmail.com>
2026-08-02 14:57:55 +0530

    again updated trust policy

--- terraform/bootstrap/iam.tf
+++ terraform/bootstrap/iam.tf
@@ -14,6 +14,7 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
       ]
     }
 
+    # Audience
     condition {
       test     = "StringEquals"
       variable = "token.actions.githubusercontent.com:aud"
@@ -23,8 +24,9 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
       ]
     }
 
+    # GitHub OIDC subject
     condition {
-      test     = "StringEquals"
+      test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
 
       values = [
@@ -32,16 +34,9 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
 
         "repo:${var.github_owner}@${var.github_owner_id}/${var.github_repository}@${var.github_repository_id}:ref:refs/heads/main",
 
+        "repo:${var.github_owner}@${var.github_owner_id}/${var.github_repository}@${var.github_repository_id}:ref:refs/heads/feature/terraform-pipeline",
+
         "repo:${var.github_owner}@${var.github_owner_id}/${var.github_repository}@${var.github_repository_id}:environment:dev"
-      ]
-    }
-
-    condition {
-      test     = "StringLike"
-      variable = "token.actions.githubusercontent.com:sub"
-
-      values = [
-        "repo:${var.github_owner}@${var.github_owner_id}/${var.github_repository}@${var.github_repository_id}:ref:refs/heads/feature/*"
       ]
     }
   }
